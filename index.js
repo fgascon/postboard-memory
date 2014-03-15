b24cddb5838d0934e52fc5636be52e8fab287785
Frédéric Gascon <frederic.gascon@gmail.com>
2014-03-14 23:08:51 -0400

    eclipse format

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var postboard = require('postboard');
 
-module.exports = function(){
-  var data = {};
-  return postboard({
-    get: function(key, callback){
-      callback(null, data[key]);
-    },
-    set: function(key, value, callback){
-      data[key] = value;
-      callback();
-    },
-    'delete': function(key, callback){
-      delete data[key];
-      callback();
-    }
-  });
-};
+module.exports = function() {
+	var data = {};
+	return postboard({
+		get: function(key, callback) {
+			callback(null, data[key]);
+		},
+		set: function(key, value, callback) {
+			data[key] = value;
+			callback();
+		},
+		'delete': function(key, callback) {
+			delete data[key];
+			callback();
+		}
+	});
+}; 
+
